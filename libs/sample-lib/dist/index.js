31e1b3f4b2693b54d9ac21fc3fe57b1e733cf0e0
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2024-06-30 01:17:02 +0530

    undo

--- libs/sample-lib/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.addNumbers = void 0;
-const addNumbers = (a, b) => {
-    return a + b;
-};
-exports.addNumbers = addNumbers;
