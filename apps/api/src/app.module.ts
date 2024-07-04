fdad6c236ded036b0e5caa2b9bf294ae1b68666d
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2024-07-05 01:02:11 +0530

    feat(api): almost full setup of backend, except payments

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -8,14 +8,27 @@ import { ThrottlerModule } from '@nestjs/throttler';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './common/prisma/prisma.module';
 import { RequestLoggerMiddleware } from './middlewares/request-logger.middleware';
-import { UsersModule } from './users/users.module';
+import { AddressesModule } from './models/addresses/addresses.module';
+import { AdminsModule } from './models/admins/admins.module';
+import { BookingTimelinesModule } from './models/booking-timelines/booking-timelines.module';
+import { BookingsModule } from './models/bookings/bookings.module';
+import { CompaniesModule } from './models/companies/companies.module';
+import { CustomersModule } from './models/customers/customers.module';
+import { GaragesModule } from './models/garages/garages.module';
+import { ManagersModule } from './models/managers/managers.module';
+import { ReviewsModule } from './models/reviews/reviews.module';
+import { SlotsModule } from './models/slots/slots.module';
+import { UsersModule } from './models/users/users.module';
+import { ValetAssignmentsModule } from './models/valet-assignments/valet-assignments.module';
+import { ValetsModule } from './models/valets/valets.module';
+import { VerificationsModule } from './models/verifications/verifications.module';
 import { configSchema } from './utils/env/config/config.schema';
 import { EnvModule } from './utils/env/env.module';
 import { EnvService } from './utils/env/env.service';
 import { HealthModule } from './utils/health/health.module';
 import { LoggerModule } from './utils/logger/logger.module';
-import { PrismaModule } from './utils/prisma/prisma.module';
 
 @Module({
   imports: [
@@ -33,7 +46,8 @@ import { PrismaModule } from './utils/prisma/prisma.module';
       inject: [EnvService], // Inject EnvService to use it for configuration
       useFactory: async (configService: EnvService) => {
         // Extract JWT_SECRET and JWT_MAX_AGE from the environment config
-        const { JWT_SECRET, JWT_MAX_AGE } = configService.envConfig;
+        const { envConfig } = configService;
+        const { JWT_SECRET, JWT_MAX_AGE } = envConfig;
         // Return the JWT configuration object
         return {
           global: true,
@@ -61,10 +75,27 @@ import { PrismaModule } from './utils/prisma/prisma.module';
       },
     ]),
     EnvModule,
-    PrismaModule,
-    HealthModule,
     LoggerModule,
+    HealthModule,
+
+    PrismaModule,
+
+    // StripeModule,
+
     UsersModule,
+    AdminsModule,
+    CustomersModule,
+    ManagersModule,
+    ValetsModule,
+    CompaniesModule,
+    GaragesModule,
+    AddressesModule,
+    SlotsModule,
+    BookingsModule,
+    ValetAssignmentsModule,
+    BookingTimelinesModule,
+    ReviewsModule,
+    VerificationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
